e25acbbcba790ae43c515d5ce79308b81ef9f125
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-03-16 14:06:14 -0500

    actividades tareas - eventos react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Tarea from './Tarea';
+import moment from 'moment';
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state={tasks:this.props.tasks}
+    moment.locale('es');
   }
 
   addTask = (task) => {
@@ -17,23 +19,60 @@ class App extends Component {
     })
   }
 
+  removeTask = (index) => {
+    console.log(`Tarea: ${index+1} removida`)
+    /* Propio - uso de estado actual */
+    const tempTasks = this.state.tasks;
+    tempTasks.splice(index,1);
+    this.setState({tasks:tempTasks});
+    
+    /* NextU - Uso estado previo (prevState) 
+    this.setState((prevState) => {
+      const tempTasks = prevState.tasks;
+      tempTasks.splice(index,1);
+      return {tasks:tempTasks}
+    })
+    */
+  }
+
+  completeTask = (index) => {
+    const tempTasks = this.state.tasks;
+    tempTasks[index].complete = true;
+    this.setState({tasks:tempTasks});
+  }
+
+  manejoOnClick = (e, index) => {
+    if (e.target.id === 'remove') this.removeTask(index);
+    else if (e.target.id === 'complete') this.completeTask(index);
+  }
+
   render() {
     const renTask = this.state.tasks.map((task,index) => {
+      var styleStatus = ''
+      styleStatus = task.complete ? 'text-success' : styleStatus;
       return (
           <div className='row p-1' key={index}>
-            <div className='col-8 ' >{task.actividad}</div>
-            <div className='col-2' >{task.fechaIni}</div>
-            <div className='col-2' >{task.fechaFin}</div>
+            <div className={'col-6 '+styleStatus} >{task.actividad}</div>
+            <div className={'col-2 '+styleStatus} >{task.fechaIni}</div>
+            <div className={'col-2 '+styleStatus} >{task.fechaFin}</div>
+            <div className='col-2'>
+              <div className='justify-content-end mx-0'>
+              <span className='fa fa-check pr-2' id='complete'
+                onClick={(e) => this.manejoOnClick(e, index)}></span>
+              <span className='fa fa-remove pr-2' id='remove'
+                onClick={(e) => this.manejoOnClick(e, index)}></span>
+              </div>
+            </div>
           </div>
       )
     });
     return (
-      <div className="conteiner-fluid border rounded m-2">
+      <div className="container-fluid border rounded m-2">
         <div className='row border-bottom m-0' >
           <div className='col-12 h1 text-center'>Lista de Tareas</div>
         </div>
         {renTask}
-        <Tarea addTask={this.addTask}/>
+        
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ const tasks = [
     {actividad:'Tarea 1', fechaIni:'19/08/2018', fechaFin:'20/08/2018',complete:false},
     {actividad:'Tarea 2', fechaIni:'16/08/2018', fechaFin:'20/08/2018',complete:false},
     {actividad:'Tarea 3', fechaIni:'01/08/2018', fechaFin:'08/08/2018',complete:false},
-    {actividad:'Tarea 4', fechaIni:'19/07/2018', fechaFin:'29/07/2018',complete:false}
+    {actividad:'Tarea 4', fechaIni:'19/07/2018', fechaFin:'29/07/2018',complete:false},
+    {actividad:'Tarea 5', fechaIni:'01/10/2019', fechaFin:'08/08/2018',complete:false},
+    {actividad:'Tarea 6', fechaIni:'12/02/2020', fechaFin:'29/07/2018',complete:false},
+    {actividad:'Tarea 7', fechaIni:'20/11/2020', fechaFin:'08/08/2018',complete:false},
+    {actividad:'Tarea 8', fechaIni:'07/04/2019', fechaFin:'29/07/2018',complete:false}
 ]
 
 ReactDOM.render(
